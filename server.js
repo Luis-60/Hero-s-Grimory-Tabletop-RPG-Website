087904f1a09d04c71b900c1aa50813b7c3d85643
Luís Carlos G. <137448502+Luis-60@users.noreply.github.com>
2024-11-14 23:19:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 const db = require('./server.js');
-
-const crypto = require('crypto')
-const DADOS_CRIPTOGRAFAR = {
-    algoritmo: 'aes-256-cbc', // Usando aes-256-cbc, que requer um IV
-    segredo: 'chavesseguracom32caracteres!', // Uma chave de 32 bytes para aes-256
-    tipo: 'hex'
-};
+const session = require("express-session")
+// const flash = require("connect-flash")
 
 const app = express();
 const port = 3000;
@@ -16,31 +11,39 @@ const port = 3000;
 const pool = new Pool ({
     user: 'postgres',
     host: '127.0.0.1',
-    database:'Hero-Grimory',
-    password: '31201013',
+    database:'postgres', // Hero-Grimory
+    password: '123',    // 31201013
     port: 5432,
 });
+
+// Sessao
+app.use(session({
+    secret: "Hero-Grimory",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {secure: false},
+}));
+
+app.get("/", function(req, res){
+    req.session.name = "Hero"
+    return res.send("Session-Set")
+});
+
+app.get("/session", function(req, res) {
+    var name = req.session.name
+    return res.send(name)
+
+    });
 
 app.use(bodyParser.json());
 
 app.post('/submit', async (req, res) => {
     const { nome, email, senha} = req.body;
-    const iv = crypto.randomBytes(16);
-    
-    // Garantir que a chave tenha 32 bytes (256 bits) usando createHash
-    const key = crypto.createHash('sha256').update(DADOS_CRIPTOGRAFAR.segredo).digest();
-
-    // Crie o objeto Cipher com o algoritmo, chave e IV
-    const cipher = crypto.createCipheriv(DADOS_CRIPTOGRAFAR.algoritmo, key, iv);
-    let senhaCriptografada = cipher.update(senha, 'utf8', DADOS_CRIPTOGRAFAR.tipo);
-    senhaCriptografada += cipher.final(DADOS_CRIPTOGRAFAR.tipo);
-
-    // Combine o IV com a senha criptografada
-    const senhaComIV = iv.toString(DADOS_CRIPTOGRAFAR.tipo) + ':' + senhaCriptografada;
     try{
         const result = await pool.query(
             'INSERT INTO Usuarios(Nome, Email, Senha) VALUES ($1, $2, $3)',
-            [nome, email, senhaComIV]
+            [nome, email, senha]
+
         );
         res.status(200).send('Dados inseridos com sucesso');
     } catch(err){
@@ -49,31 +52,24 @@ app.post('/submit', async (req, res) => {
     }
 });
 app.post('/login', async (req, res) => {
-    const { nome, senha } = req.body;
-    const iv = crypto.randomBytes(16);
-
-    // Garantir que a chave tenha 32 bytes (256 bits) usando createHash
-    const key = crypto.createHash('sha256').update(DADOS_CRIPTOGRAFAR.segredo).digest();
-
-    // Crie o objeto Cipher com o algoritmo, chave e IV
-    const cipher = crypto.createCipheriv(DADOS_CRIPTOGRAFAR.algoritmo, key, iv);
-    let senhaCriptografada = cipher.update(senha, 'utf8', DADOS_CRIPTOGRAFAR.tipo);
-    senhaCriptografada += cipher.final(DADOS_CRIPTOGRAFAR.tipo);
-
-    // Combine o IV com a senha criptografada
-    const senhaComIV = iv.toString(DADOS_CRIPTOGRAFAR.tipo) + ':' + senhaCriptografada;
+ const { nome, senha } = req.body;
  try{
 const result = await pool.query(
     'SELECT * FROM Usuarios WHERE nome = $1 AND senha = $2',
-    [nome, senhaComIV]
+    [nome, senha]
 );
 if (result.rows.length > 0) {
     const usuario = result.rows[0];
+    var confirmacao = false;
 
     if (usuario.senha === senha) {
         res.status(200).json({ message: 'Login realizado com sucesso'});
+        confirmacao = true;
+        module.exports = confirmacao
     } else {
         res.status(401).json({ message: 'Senha incorreta!'})
+        confirmacao = false;
+        module.exports = confirmacao
     }   
 } else {
     res.status(404).json({ message: 'Usuário não encontrado!'});
